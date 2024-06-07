634088aad35e35f1a5ac72102e731acbde625127
mohammadpiriyan <mohammadpiriyan@gmail.com>
2024-06-07 18:45:43 +0330

    add /register & /userlist & /update & /getbyid & patch user to routs

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,25 @@
 const express = require("express");
-const { getUserList, postUserList } = require("../controllers/usercontroller");
-
+const { asyncHandler } = require("../utils/async-handler");
+const { getUserList, postUserList, registerUser, getAllUser, updateUser, handleGetById,editUserById } = require("../controllers/usercontroller");
 const app = express();
 
 app.get("/getlist", getUserList);
+
 app.post("/postlist", postUserList);
 
+app.post("/register", registerUser);
+
+app.get("/userlist", getAllUser)
+
+app.put("/update", updateUser)
+
+app.get("/getbyid", handleGetById)
+
+// app.put("/editUser", editUserById)
+app.patch(
+    "/:id",
+    asyncHandler(editUserById)
+  );
 
 
 module.exports = app
